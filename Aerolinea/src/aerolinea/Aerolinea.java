4ee4e7aab0c743dd64ebc70bed61a50cf5703b26
Jagz <Usuario@DESKTOP-MG2KIDD>
2020-12-02 13:31:23 -0700

    codigoTXT3-AVISOS

--- Aerolinea/src/aerolinea/Aerolinea.java
+++ Aerolinea/src/aerolinea/Aerolinea.java
@@ -22,18 +22,31 @@ public class Aerolinea {
     
     public static void main(String[] args) {
         // TODO code application logic here
+        /*
+        El codigo te pregunta que si quieres ver el aeropuerto destino o
+        origen (Pienso dejar unicamente, origen), (NOTA: lo que sigue es si
+        seleccionas "origen") despues entra en el metodo "verDia()" y luego
+        de seleccionar un dia, te pide el aeropuerto y aqui, si pulsas "0" 
+        imprime mas nombres de aeropuertos, copia y pega el nombre. Finalmente
+        imprime el nombre del aeropuerto que pegaste, solo que, lo busca en el 
+        archivo que especificaste con los metodos "menu()" y "verDia()", y se 
+        imprime cada vez que encuentra una linea con ese nombre. 
+        
+        NOTA:El codigo ahora debe usar la variable "aeropuerto" para futuras
+        busquedas como la de "destino", el problema es la codificacion, no
+        guarda acentos. Intenten repararlo.
+        */
+        
+        
         
         System.out.println("Aerolinea ISI:");
         int respuesta = 0;
         String opcion = "";  
         String aeropuerto = "";
         
-        archivos a = new archivos();
+        archivos a = new archivos();       
+        respuesta = a.menu(); //regresa "respuesta" y lo recibe "verDia()"
         
-        //aeropuerto = a.seleccionaAeropuerto(respuesta); 
-        //System.out.println("");
-        //System.out.println("");
-        respuesta = a.menu();
         
         switch(respuesta){
             
--- Aerolinea/src/aerolinea/archivos.java
+++ Aerolinea/src/aerolinea/archivos.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyectofc3;
+package aerolinea;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -194,7 +194,9 @@ public class archivos {
     }
     
     
-    public String seleccionaAeropuerto(int resp){ //Este metodo esta completo
+    public String seleccionaAeropuerto(int resp){ //NOTA:El metodo tiene problemas de codificacion y no guarda los acentos
+        //NOTA2:Al no poder guardar acentos, ocasiona conflictos con las futuras busquedas, intenten repararlo
+        
         //el chiste es que tome la opcion del usuario y guarde esa opcion y con eso le indique
         //al metodo "aeropuertoDestino" y "aeropuertoOrigen" que aeropuertos deben imprimir
         String aeropuerto = "";
